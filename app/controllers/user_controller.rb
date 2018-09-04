00faf7baede8a13180b4979ec6c28f16b70c2cb2
GustavoCig <gustavocignachi@hotmail.com>
2018-09-04 13:33:20 -0300

    Adds 'authenticated' and 'unauthenticated' routes and also 'name' field to the registration forms.
    Commit: Adds
    
    TaskNumber: -

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,46 @@
-class UserController < ApplicationController
-   
-    def index
-    end
+class UserController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
 
-    def teste
-        redirect_to "/users/sign_in"
-    end
+  def index
+  end
+
+  def new
+    super
+  end
+
+  # POST /resource
+  def create
+    super
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "user"}
   devise_scope :user do
     get '/logout/' => 'devise/sessions#destroy'
+
+    unauthenticated do
+      root :to => 'devise/sessions#create'
+    end
+
+    authenticated do
+      root :to => 'user#index'
+    end
   end
-  root to: 'user#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
